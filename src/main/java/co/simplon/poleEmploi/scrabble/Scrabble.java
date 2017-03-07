54a3010deb15d88f36b280109cff1597a641e5a9
valsimplon <val.simplon@gmail.com>
2017-03-07 16:43:48 +0100

    1er test ok

--- src/main/java/co/simplon/poleEmploi/scrabble/Scrabble.java
+++ src/main/java/co/simplon/poleEmploi/scrabble/Scrabble.java
@@ -3,12 +3,59 @@ package co.simplon.poleEmploi.scrabble;
 public class Scrabble {
 
 	public int getValeur(char c) {
-		// TODO Auto-generated method stub
-		return 0;
+		int valeur;
+		switch (c) {
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'L':
+		case 'N':
+		case 'O':
+		case 'R':
+		case 'S':
+		case 'T':
+		case 'U':
+			valeur = 1;
+			break;
+		case 'B':
+		case 'C':
+		case 'P':
+			valeur = 3;
+			break;
+		case 'D':
+		case 'G':
+		case 'M':
+			valeur = 2;
+			break;
+		case 'F':
+		case 'H':
+		case 'V':
+			valeur = 4;
+			break;
+		case 'J':
+		case 'Q':
+			valeur = 8;
+			break;
+		case 'K':
+		case 'W':
+		case 'X':
+		case 'Y':
+		case 'Z':
+			valeur = 10;
+			break;
+		default:
+			valeur = 0;
+			break;
+		}
+		return valeur;
 	}
 
 	public int getValeur(String string) {
-		// TODO Auto-generated method stub
+//		int valeurMot = 0;
+//		
+//		for (int i = 0; i < string.length(); i++) {
+//			valeurMot += getValeur(string[i]);
+//		}
 		return 0;
 	}
 
